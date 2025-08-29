c299809eccbe4d7c63494de618e6b9dcfbafabd6
TheBaytur <bayturcodding@gmail.com>
2025-08-29 15:54:31 +0800

    widget_test

--- lib/widgets/user_profile.dart
+++ lib/widgets/user_profile.dart
@@ -5,10 +5,46 @@ class UserProfile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Center(
-        child: Text('User Profile'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings'),
+      ),
+      body: Container(
+        color: Colors.white,
+        child: Container(          
+          width: double.infinity,
+          child: Column(            
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Container(               
+                width: 100,
+                height: 100,
+                child: Placeholder(
+                  // fallbackHeight: 200,
+                  // fallbackWidth: 200,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text('Vasya Pupkin'),
+              SizedBox(
+                height: 10,
+              ),
+              Text('+1 (234) 567-89-00'),
+              SizedBox(
+                height: 10,
+              ),
+              Text('New York, USA'),
+              SizedBox(
+                height: 10,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }

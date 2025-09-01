4e3132091e510df26b0aa93c6378c7aa56cae022
TheBaytur <bayturcodding@gmail.com>
2025-09-01 15:10:43 +0800

    1 sep

--- lib/widgets/user_profile.dart
+++ lib/widgets/user_profile.dart
@@ -14,18 +14,34 @@ class UserProfile extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              SizedBox(height: 20),
-              _AvatarWidget(),
-              SizedBox(height: 20),
-              _UserNameWidget(),
-              SizedBox(height: 10),
-              _UserPhoneWidget(),
-              SizedBox(height: 10),
-              _UserAddressWidget(),
-              SizedBox(height: 10),
+              _UserInfo(),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class _UserInfo extends StatelessWidget {
+  const _UserInfo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Column(
+        children: [
+          SizedBox(height: 20),
+                _AvatarWidget(),
+                SizedBox(height: 20),
+                _UserNameWidget(),
+                SizedBox(height: 10),
+                _UserPhoneWidget(),
+                SizedBox(height: 10),
+                _UserAddressWidget(),
+                SizedBox(height: 10),
+        ],
       ),
     );
   }
@@ -70,6 +86,8 @@ class _UserNameWidget extends StatelessWidget {
     );
   }
 }
+
+
 
 class _AvatarWidget extends StatelessWidget {
   const _AvatarWidget({super.key});
